39ff9592efdbd66b16881aadd8efce425b4a9702
Jan Schulte <hello@unexpected-co.de>
2018-04-02 11:54:26 +0200

    Fetch and render build status

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,10 @@ import * as vscode from 'vscode';
 import { StatusBarItem, window, workspace } from 'vscode';
 import * as Path from 'path';
 import * as ghslug from 'github-slug';
+import * as request from 'request';
 
 export function activate(context: vscode.ExtensionContext) {
-
-
+    let travisStatusBar = new TravisStatusBar();
     let disposable = vscode.commands.registerCommand('extension.travis-status', () => {
         window.showInputBox({
             placeHolder: "Configure your Travis CI token",
@@ -15,12 +15,12 @@ export function activate(context: vscode.ExtensionContext) {
         .then(token => {
             if(token) {
                 window.showInformationMessage("Thank you. Checking status now");
-                let travisStatusBar = new TravisStatusBar(token);
-                context.subscriptions.push(travisStatusBar);
+                travisStatusBar.updateBuildStatus(token);
             }
         });
     });
 
+    context.subscriptions.push(travisStatusBar);
     context.subscriptions.push(disposable);
 }
 
@@ -29,17 +29,20 @@ export function deactivate() {
 
 class TravisStatusBar {
     private statusBarItem : StatusBarItem;
-    private token: String;
+    private token: String | null;
+    private slug: String | null;
 
-    constructor(token: String) {
+    constructor() {
         this.statusBarItem = window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-        this.token = token;
+        this.token = null;
+        this.slug = null;
     }
 
-    public updateBuildStatus() {
+    public updateBuildStatus(token: String) {
         if(!this.statusBarItem) {
             return;
         }
+        this.token = token;
 
         this.statusBarItem.text = "Build Status: Unknown";
         this.statusBarItem.show();
@@ -48,11 +51,13 @@ class TravisStatusBar {
             let sourceControl = vscode.scm.createSourceControl("git", "git", workspace.workspaceFolders[0].uri);
             if(sourceControl.rootUri) {
                 const gitPath = Path.join(sourceControl.rootUri.fsPath.toString(), ".git", "config");
-                ghslug(gitPath, function(error: Error, slug: String) {
+                ghslug(gitPath, (error: Error, slug: String) => {
                     if(error) {
                         console.error(error);
                     } else {
-                        console.log(slug);
+                        this.slug = slug;
+                        console.log('fetching status');
+                        this.fetchStatus();
                     }
                 });
             }
@@ -61,5 +66,32 @@ class TravisStatusBar {
 
     dispose() {
         this.statusBarItem.dispose();
+    }
+
+    private fetchStatus() {
+        if(this.slug) {
+            const urlSafeSlug = this.slug.replace("/", "%2F");
+
+            const options = {
+                url: `https://api.travis-ci.com/repo/${urlSafeSlug}/builds?limit=1`,
+                headers: {
+                  'Authorization': `token ${this.token}`,
+                  'Travis-API-Version': '3',
+                  'User-Agent': 'API Explorer'
+                }
+              };
+
+            request.get(options,(error, response) => {
+                if(response.statusCode === 200) {
+                    const buildResponse = JSON.parse(response.body);
+                    if (buildResponse.builds.length > 0) {
+                        this.statusBarItem.text = `Build Status: ${buildResponse.builds[0].state}`;
+                    } else {
+                        this.statusBarItem.text = "Build Status: unknown";
+                    }
+                    this.statusBarItem.show();
+                }
+            });
+        }
     }
 }
