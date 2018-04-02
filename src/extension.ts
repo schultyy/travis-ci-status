9dbbf2404d8daed94c0e62b266bed3c5c019dd80
Jan Schulte <hello@unexpected-co.de>
2018-04-02 08:48:52 +0200

    render status bar item

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { StatusBarItem, window } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,6 +12,8 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "travis-ci-status" is now active!');
 
+    let travisStatusBar = new TravisStatusBar();
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
@@ -19,11 +22,30 @@ export function activate(context: vscode.ExtensionContext) {
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
+        travisStatusBar.updateBuildStatus();
     });
 
+    context.subscriptions.push(travisStatusBar);
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class TravisStatusBar {
+    private statusBarItem : StatusBarItem;
+
+    public updateBuildStatus() {
+        if(!this.statusBarItem) {
+            this.statusBarItem = window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+        }
+
+        this.statusBarItem.text = "Build Status: Unknown";
+        this.statusBarItem.show();
+    }
+
+    dispose() {
+        this.statusBarItem.dispose();
+    }
 }
