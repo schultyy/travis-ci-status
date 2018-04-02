a600cc6838b2b27a7163501dba33d9df444cc772
Jan Schulte <hello@unexpected-co.de>
2018-04-02 17:35:16 +0200

    Configure token for .org

--- src/extension.ts
+++ src/extension.ts
@@ -3,17 +3,28 @@ import * as vscode from 'vscode';
 import TravisStatusBar from './TravisStatusBar';
 
 const STATE_COM_API_KEY = 'travis-ci-com-api-key';
+const STATE_ORG_API_KEY = 'travis-ci-org-api-key';
 
-const configureToken = (travisStatusBar: TravisStatusBar, context: vscode.ExtensionContext) => {
-    vscode.window.showInputBox({
-        placeHolder: "Configure your travis-ci.com token",
-        prompt: "Configure your travis-ci.com token"
-    })
-    .then(token => {
-        if(token) {
-            context.globalState.update(STATE_COM_API_KEY, token);
-            travisStatusBar.updateBuildStatus(token);
-        }
+enum TravisEnvironment {
+    com,
+    org
+}
+
+const configureToken = (travisStatusBar: TravisStatusBar, env: TravisEnvironment, context: vscode.ExtensionContext) => {
+    let environmentStr = '';
+
+    switch(env) {
+        case TravisEnvironment.com:
+            environmentStr = 'travis-ci.com';
+            break;
+        case TravisEnvironment.org:
+            environmentStr = 'travis-ci.org';
+            break;
+    }
+
+    return vscode.window.showInputBox({
+        placeHolder: `Configure your ${environmentStr} token`,
+        prompt: `Configure your ${environmentStr} token`
     });
 };
 
@@ -21,7 +32,7 @@ let travisStatusBar = new TravisStatusBar();
 
 export function activate(context: vscode.ExtensionContext) {
 
-    let disposableStatus = vscode.commands.registerCommand('extension.travis-status', () => {
+    let disposableComStatus = vscode.commands.registerCommand('extension.travis-status', () => {
         const apiToken = context.globalState.get(STATE_COM_API_KEY);
         if (apiToken) {
             travisStatusBar.updateBuildStatus(apiToken.toString());
@@ -30,13 +41,30 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
-    let disposableConfigureToken = vscode.commands.registerCommand('extension.configure-travis-ci-com-token',() => {
-         configureToken(travisStatusBar, context);
+    let disposableConfigureComToken = vscode.commands.registerCommand('extension.configure-travis-ci-com-token',() => {
+         configureToken(travisStatusBar, TravisEnvironment.com, context)
+         .then(token => {
+            if(token) {
+                context.globalState.update(STATE_COM_API_KEY, token);
+                travisStatusBar.updateBuildStatus(token);
+            }
+        });
     });
 
+    let disposableConfigureOrgToken = vscode.commands.registerCommand('extension.configure-travis-ci-org-token',() => {
+        configureToken(travisStatusBar, TravisEnvironment.org, context)
+        .then(token => {
+            if(token) {
+                context.globalState.update(STATE_ORG_API_KEY, token);
+                travisStatusBar.updateBuildStatus(token);
+            }
+        });
+   });
+
     context.subscriptions.push(travisStatusBar);
-    context.subscriptions.push(disposableStatus);
-    context.subscriptions.push(disposableConfigureToken);
+    context.subscriptions.push(disposableComStatus);
+    context.subscriptions.push(disposableConfigureComToken);
+    context.subscriptions.push(disposableConfigureOrgToken);
 }
 
 export function deactivate() {
