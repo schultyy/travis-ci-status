d8696c1ba13e8d40ddf191f40eadd93355569d0c
Jan Schulte <hello@unexpected-co.de>
2018-04-02 12:29:18 +0200

    Add second command to override saved API token

--- src/extension.ts
+++ src/extension.ts
@@ -5,23 +5,37 @@ import * as Path from 'path';
 import * as ghslug from 'github-slug';
 import * as request from 'request';
 
+const configureToken = (travisStatusBar: TravisStatusBar, context: vscode.ExtensionContext) => {
+    window.showInputBox({
+        placeHolder: "Configure your Travis CI token",
+        prompt: "Configure your Travis CI token"
+    })
+    .then(token => {
+        if(token) {
+            context.globalState.update('travis-ci-api-key', token);
+            travisStatusBar.updateBuildStatus(token);
+        }
+    });
+};
+
 export function activate(context: vscode.ExtensionContext) {
     let travisStatusBar = new TravisStatusBar();
-    let disposable = vscode.commands.registerCommand('extension.travis-status', () => {
-        window.showInputBox({
-            placeHolder: "Configure your Travis CI token",
-            prompt: "Configure your Travis CI token"
-        })
-        .then(token => {
-            if(token) {
-                window.showInformationMessage("Thank you. Checking status now");
-                travisStatusBar.updateBuildStatus(token);
-            }
-        });
+    let disposableStatus = vscode.commands.registerCommand('extension.travis-status', () => {
+        const apiToken = context.globalState.get('travis-ci-api-key');
+        if (apiToken) {
+            travisStatusBar.updateBuildStatus(apiToken.toString());
+        } else {
+            configureToken(travisStatusBar, context);
+        }
+    });
+
+    let disposableConfigureToken = vscode.commands.registerCommand('extension.configure-travis-ci-token',() => {
+         configureToken(travisStatusBar, context);
     });
 
     context.subscriptions.push(travisStatusBar);
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposableStatus);
+    context.subscriptions.push(disposableConfigureToken);
 }
 
 export function deactivate() {
